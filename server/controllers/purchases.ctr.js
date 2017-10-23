40c8e80114482b5203a8f737ebce936a2cad98ed
Luke Robinson <lukerobinson@lukes-mbp.chattanoogatn.gov>
2017-10-23 16:45:24 -0400

    more change

---
+++ server/controllers/purchases.ctr.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var procedures = require('../procedures/purchases.proc');
+var auth = require('../middleware/auth.mw');
+
+var router = express.Router();
+
+
+
+
+
+
+module.exports = router;
--- server/procedures/purchases.proc.js
+++ server/procedures/purchases.proc.js
@@ -1,1 +1,9 @@
 var db = require('../config/db');
+
+exports.allPurchases = function() {
+    return db.rows('getPurchases');
+}
+
+exports.addPurchase = function(productid, price, stripeid) {
+    return db.row('insertPurchase', [productid, price, stripeid]);
+}
