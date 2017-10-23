a48e0a57fcd08864b5ad67eee55f65235a27f5bf
Vernique Norwood <vnorw@Verniques-MacBook-Air.local>
2017-10-23 14:02:47 -0400

    newest update html

--- client/app.js
+++ client/app.js
@@ -1,1 +1,8 @@
-angular.module ('onlinestore ') ['ngRoute']
+angular.module('onlinestore',  ['ngRoute', 'ngResource', 'onlinestore.controllers', 'onlinestore'])
+.config(['$routeProvider', function($routeProvider) {
+    $routerProvider
+    .when('/', {
+        templateUrl: 'views/welcome.html'
+    })
+    .when('/')
+}])
