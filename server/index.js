f25a9bf5df2f290259c856008d3f3d14643f9302
Luke Robinson <lukerobinson@lukes-mbp.chattanoogatn.gov>
2017-10-23 13:54:27 -0400

    db.js and index.js

---
+++ server/index.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var clientPath = path.join(__dirname, '../client');
+var mysql = require('mysql');
+
+var api = require('./api');
+var cookieParser = require('cookie-parser');
+var configurePassport = require('./config/passport');
+
+var prerender = require('prerender-node');
+
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "",
+    user: "",
+    password: "",
+    database: ""
+});
+
+// prerender.set('prerenderToken', process.env.PRERENDER_TOKEN);
+// app.use(prerender);
+
+app.use(express.static(clientPath));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+configurePassport(app);
+
+app.use('/api', api);
+
+function isAsset(path) {
+    var pieces = path.split('/');
+    if (pieces.length === 0) { return false; }
+    var last = pieces[pieces.length - 1];
+    if (path.indexOf('/api') !== -1 || path.indexOf('/?') !== -1) {
+        return true;
+    } else if (last.indexOf('.') !== -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+app.get('*', function(req, res, next) {
+    if (isAsset(req.url)) {
+        return next();    
+    } else {        
+        res.sendFile(path.join(clientPath, 'index.html'));    
+    }
+}); 
+
+
+app.listen(3000);
