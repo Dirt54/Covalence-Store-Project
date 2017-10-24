eb922e63f1da8113418eda8feb0802885f7ad88d
Vernique Norwood <vnorw@Verniques-MacBook-Air.local>
2017-10-24 11:50:19 -0400

    changes to style

--- client/controllers.js
+++ client/controllers.js
@@ -1,15 +1,15 @@
 angular.module('store.controllers', [])
 
-    .controller('welcomeController', ['$scope', '$location', '$routeParams', 'UserService', 'SEOService', function ($scope, $location, $routeparams, UserService, SEOService) {
+    .controller('welcomeController', ['$scope', '$location', '$routeParams', 'SEOService', function ($scope, $location, $routeparams,  SEOService) {
 
 
     }])
 
-    .controller('apparelController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams', 'UserService', 'SEOService', function ($scope, Products, Purchases, $location, $routeparams, UserService, SEOService) {
+    .controller('apparelController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams',  'SEOService', function ($scope, Products, Purchases, $location, $routeparams,  SEOService) {
 
     }])
 
-    .controller('miscController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams', 'UserService', 'SEOService', function ($scope, Products, Purchases, $location, $routeparams, UserService, SEOService) {
+    .controller('miscController', ['$scope', 'Products', 'Purchases', '$location', '$routeParams',  'SEOService', function ($scope, Products, Purchases, $location, $routeparams, SEOService) {
 
     }])
 
